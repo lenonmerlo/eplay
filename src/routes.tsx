06da7d3f996627607b5745c9d4515d29d7178847
lenonmerlo <lenontm@gmail.com>
2024-10-17 14:57:19 -0300

    construção do hero

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './components/pages/Home'
 import Categories from './components/pages/Categories'
+import Product from './components/pages/Product'
 
 const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/categories" element={<Categories />} />
+    <Route path="/product/:id" element={<Product />} />
   </Routes>
 )
 
